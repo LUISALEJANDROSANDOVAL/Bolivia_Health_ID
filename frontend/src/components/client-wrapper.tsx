9277a6b2edd1e3b2e77e2ab352ff7bbe56046468
Eddy <eddyygalvaan@gmail.com>
2026-06-18 11:31:31 -0400

    Descripcion de cambios

--- frontend/src/components/client-wrapper.tsx
+++ frontend/src/components/client-wrapper.tsx
@@ -1,7 +1,12 @@
 'use client'
 
 import React from 'react'
-import { Providers } from './providers'
+import dynamic from 'next/dynamic'
+
+const Providers = dynamic(
+  () => import('./providers').then((mod) => mod.Providers),
+  { ssr: false }
+)
 
 export function ClientWrapper({ children }: { children: React.ReactNode }) {
   return <Providers>{children}</Providers>
